205cf613c1959de0c2850457b1c11ed677066293
Lefteris Eleftheriadis <lefteriselefte@gmail.com>
2025-01-05 15:31:00 +0200

    Create partials and custom routes

--- helpers.php
+++ helpers.php
@@ -10,3 +10,64 @@ function basePath($path = ''): string
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found'";
+    }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partoa; '{$name} not found'";
+    }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value): void
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value): void
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,7 +1,6 @@
 <?php
 loadPartial('head');
 loadPartial('navbar');
-loadPartial('showcase-search');
 loadPartial('top-banner');
 ?>
 
@@ -171,10 +170,7 @@ loadPartial('top-banner');
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
+
 </section>
 
 
